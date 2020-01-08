c45bef39c121c86185f91ba50842875b1a6822c6
Mauricio Coria <coriamauricio@gmail.com>
2020-01-08 09:02:31 -0300

    Adding ReyMoveGeneratorTest

---
+++ src/test/java/movegenerators/ReyMoveGeneratorTest.java
@@ -0,0 +1,14 @@
+package movegenerators;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReyMoveGeneratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
