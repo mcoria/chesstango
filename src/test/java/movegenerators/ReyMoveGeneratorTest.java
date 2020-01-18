6f799dca9fad759f81221292bfca7138cd235767
Mauricio Coria <coriamauricio@gmail.com>
2020-01-18 10:00:55 -0300

    Adding enroque test

--- src/test/java/movegenerators/ReyMoveGeneratorTest.java
+++ src/test/java/movegenerators/ReyMoveGeneratorTest.java
@@ -32,7 +32,7 @@ public class ReyMoveGeneratorTest {
 	
 		ReyMoveGenerator moveGenerator = new ReyMoveGenerator(Color.BLANCO);
 		
-		Set<Move> moves = moveGenerator.getPseudoMoves(tablero, new SimpleImmutableEntry<Square, Pieza>(from, Pieza.REY_BLANCO));
+		Set<Move> moves = moveGenerator.getPseudoMoves(tablero, origen);
 		
 		assertEquals(8, moves.size());
 		
@@ -55,12 +55,12 @@ public class ReyMoveGeneratorTest {
 		assertEquals(Pieza.REY_BLANCO, tablero.getPieza(from));
 		assertEquals(Pieza.PEON_BLANCO, tablero.getPieza(Square.e6));
 		assertEquals(Pieza.PEON_NEGRO, tablero.getPieza(Square.e4));
-		
-		Map.Entry<Square, Pieza> origen = new SimpleImmutableEntry<Square, Pieza>(from, Pieza.REY_BLANCO);
 	
 		ReyMoveGenerator moveGenerator = new ReyMoveGenerator(Color.BLANCO);
 		
-		Set<Move> moves = moveGenerator.getPseudoMoves(tablero, new SimpleImmutableEntry<Square, Pieza>(from, Pieza.REY_BLANCO));
+		Map.Entry<Square, Pieza> origen = new SimpleImmutableEntry<Square, Pieza>(from, Pieza.REY_BLANCO);
+		
+		Set<Move> moves = moveGenerator.getPseudoMoves(tablero, origen);
 		
 		assertEquals(7, moves.size());
 		
@@ -71,6 +71,27 @@ public class ReyMoveGeneratorTest {
 		assertTrue(moves.contains( createSimpleMove(origen, Square.d4) ));
 		assertTrue(moves.contains( createCaptureMove(origen, Square.e4, Pieza.PEON_NEGRO) ));
 		assertTrue(moves.contains( createSimpleMove(origen, Square.f4) ));			
+	}
+	
+	
+	@Test
+	public void testEnroque() {
+		FENParser parser = new FENParser();
+		DummyBoard tablero = parser.parsePiecePlacement("8/8/8/8/8/8/8/R3K2R");
+		
+		Square from = Square.e1;
+		assertEquals(Pieza.REY_BLANCO, tablero.getPieza(from));
+		assertEquals(Pieza.TORRE_BLANCO, tablero.getPieza(Square.a1));
+		assertEquals(Pieza.TORRE_BLANCO, tablero.getPieza(Square.h1));
+	
+		ReyMoveGenerator moveGenerator = new ReyMoveGenerator(Color.BLANCO);
+		
+		Map.Entry<Square, Pieza> origen = new SimpleImmutableEntry<Square, Pieza>(from, Pieza.REY_BLANCO);
+		
+		Set<Move> moves = moveGenerator.getPseudoMoves(tablero, origen);
+		
+		assertEquals(7, moves.size());
+					
 	}
 	
 	private Move createSimpleMove(Entry<Square, Pieza> origen, Square destinoSquare) {
