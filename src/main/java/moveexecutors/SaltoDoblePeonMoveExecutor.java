83a8caf6118db79a40a0be4ab1c7da8694fc8935
Mauricio Coria <coriamauricio@gmail.com>
2020-01-18 09:02:12 -0300

    Implementing SaltoDoblePeonMoveExecutor

--- src/main/java/moveexecutors/SaltoDoblePeonMoveExecutor.java
+++ src/main/java/moveexecutors/SaltoDoblePeonMoveExecutor.java
@@ -1,21 +1,28 @@
 package moveexecutors;
 
 import chess.BoardState;
+import chess.Color;
 import chess.DummyBoard;
 import chess.Move;
+import chess.Square;
 
 public class SaltoDoblePeonMoveExecutor implements MoveExecutor {
 
 	@Override
 	public void execute(DummyBoard board, BoardState boardState, Move move) {
-		// TODO Auto-generated method stub
+		Square peonPasanteSquare = Square.getSquare(move.getTo().getKey().getFile(),  Color.BLANCO.equals(move.getFrom().getValue().getColor()) ? move.getTo().getKey().getRank() - 1 : move.getTo().getKey().getRank() + 1);
+		board.setEmptySquare(move.getFrom().getKey());						//Dejamos origen
+		board.setPieza(move.getTo().getKey(), move.getFrom().getValue());	//Vamos a destino
 		
+		
+		boardState.setCaptura(null);
+		boardState.setPeonPasanteSquare(peonPasanteSquare);
 	}
 
 	@Override
 	public void undo(DummyBoard board, BoardState boardState, Move move) {
-		// TODO Auto-generated method stub
-		
+		board.setEmptySquare(move.getTo().getKey());						//Reestablecemos destino
+		board.setPieza(move.getFrom());										//Volvemos a origen
 	}
 
 }
