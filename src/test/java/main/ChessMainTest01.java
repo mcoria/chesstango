1b892128b13cfd4252664507bdfc4d5b20b2a01f
Mauricio Coria <coriamauricio@gmail.com>
2020-01-18 20:42:44 -0300

    Adding divide 2

--- src/test/java/main/ChessMainTest01.java
+++ src/test/java/main/ChessMainTest01.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import chess.Board;
+import chess.Square;
 import parsers.FENParser;
 
 //Kiwipete
@@ -25,5 +26,15 @@ public class ChessMainTest01 {
 		
 		assertEquals(48, board.getMovimientosPosibles().size());
 		assertEquals(48, rootNode.getChildNodesCounter());
-	}	
+	}
+	
+	@Test
+	public void test02() {
+		Board board = FENParser.parseFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+		Node rootNode = main.start(board, 2);
+		
+		assertEquals(48, board.getMovimientosPosibles().size());
+		//assertEquals(2034, rootNode.getChildNodesCounter());
+		assertEquals(43, rootNode.getChildNode(Square.e1, Square.g1).getChildNodesCounter());
+	}		
 }
