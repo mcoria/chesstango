53143e0c06d20c0d5d3877acff074b292e2608ca
Mauricio Coria <coriamauricio@gmail.com>
2021-07-27 19:55:30 -0300

    Adding perftsuite

---
+++ src/main/java/main/PerftSuite.java
@@ -0,0 +1,84 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import builder.ChessBuilderGame;
+import chess.Game;
+import parsers.FENParser;
+
+public class PerftSuite {
+
+	public static void main(String[] args) {
+		try {
+			PerftSuite suite = new PerftSuite();
+			
+			InputStream instr = suite.getClass().getClassLoader().getResourceAsStream("main/perftsuite.txt");
+
+			// reading the files with buffered reader
+			InputStreamReader strrd = new InputStreamReader(instr);
+
+			BufferedReader rr = new BufferedReader(strrd);
+
+			String line;
+
+			// outputting each line of the file.
+			while ((line = rr.readLine()) != null) {
+				suite.run(line);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String fen;
+	protected int[] perftResults;
+
+	protected void run(String tests) {
+		parseTests(tests);
+		
+		System.out.println("Testing FEN: " + this.fen);
+		for(int i = 0; i < perftResults.length; i++){
+			
+			ChessMain main = new ChessMain();
+			
+			Node rootNode = main.start(getGame(), i + 1);
+			
+			if(rootNode.getChildNodesCounter() == perftResults[i]){
+				System.out.println("depth " + (i + 1) + " OK" );
+			} else {
+				System.out.println("depth " + (i + 1) + " FAIL" );
+				break;
+			}
+			
+		}
+		System.out.println("=============");
+		
+	}
+
+	protected void parseTests(String tests) {
+		String[] splitStrings = tests.split(";");
+		
+		this.fen = splitStrings[0].trim();
+		this.perftResults = new int[splitStrings.length - 1];
+		
+		for(int i = 1; i < splitStrings.length; i++){
+			String[] perftResultStr = splitStrings[i].split(" ");
+			perftResults[i - 1] =  Integer.parseInt(perftResultStr[1]);
+		}
+		
+	}
+	
+	private Game getGame() {		
+		ChessBuilderGame builder = new ChessBuilderGame();
+
+		FENParser parser = new FENParser(builder);
+		
+		parser.parseFEN(this.fen);
+		
+		return builder.getGame();
+	}		
+
+}
---
+++ src/test/java/main/PerftSuiteTest.java
@@ -0,0 +1,38 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PerftSuiteTest {
+	
+	private PerftSuite suite;
+	
+	@Before
+	public void setUp() {
+		suite = new PerftSuite();
+		
+	}
+	
+	@Test
+	public void test_parse() {
+		suite.parseTests("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ;D1 20 ;D2 400 ;D3 8902 ;D4 197281 ;D5 4865609 ;D6 119060324");
+		
+		assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", suite.fen);
+		
+		assertEquals(20, suite.perftResults[0]);
+		assertEquals(400, suite.perftResults[1]);
+		assertEquals(8902, suite.perftResults[2]);
+		assertEquals(197281, suite.perftResults[3]);
+		assertEquals(4865609, suite.perftResults[4]);
+		assertEquals(119060324, suite.perftResults[5]);
+		
+	}
+	
+	
+	@Test
+	public void test_execute1() {
+		suite.run("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ;D1 20 ;D2 400 ;D3 8902 ;D4 197281 ;D5 4865609 ;D6 119060324");
+	}
+}
