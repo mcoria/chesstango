a96226caffa677aaa868e107d1546fc3da6a5d6f
Mauricio Coria <coriamauricio@gmail.com>
2021-08-03 00:44:16 -0300

    Removing StrategySelector, using functional interface

--- src/main/java/chess/Pieza.java
+++ src/main/java/chess/Pieza.java
@@ -1,38 +1,44 @@
 package chess;
 
+import java.util.function.Function;
+
 import movegenerators.MoveGenerator;
 import movegenerators.MoveGeneratorStrategy;
 
 public enum Pieza {
-	PEON_BLANCO(Color.BLANCO, (MoveGeneratorStrategy strategy) -> strategy.getPeonBlancoMoveGenerator()),
-	PEON_NEGRO(Color.NEGRO, (MoveGeneratorStrategy strategy) -> strategy.getPeonNegroMoveGenerator()),
+	PEON_BLANCO(Color.BLANCO, strategy -> strategy.getPeonBlancoMoveGenerator()),
+	PEON_NEGRO(Color.NEGRO,  strategy -> strategy.getPeonNegroMoveGenerator()),
 	
-	TORRE_BLANCO(Color.BLANCO, (MoveGeneratorStrategy strategy) -> strategy.getTorreBlancaMoveGenerator()),
-	TORRE_NEGRO(Color.NEGRO, (MoveGeneratorStrategy strategy) -> strategy.getTorreNegraMoveGenerator()),
+	TORRE_BLANCO(Color.BLANCO, strategy -> strategy.getTorreBlancaMoveGenerator()),
+	TORRE_NEGRO(Color.NEGRO, strategy -> strategy.getTorreNegraMoveGenerator()),
 	
-	CABALLO_BLANCO(Color.BLANCO, (MoveGeneratorStrategy strategy) -> strategy.getCaballoBlancoMoveGenerator()),
-	CABALLO_NEGRO(Color.NEGRO, (MoveGeneratorStrategy strategy) -> strategy.getCaballoNegroMoveGenerator()),
+	CABALLO_BLANCO(Color.BLANCO, strategy -> strategy.getCaballoBlancoMoveGenerator()),
+	CABALLO_NEGRO(Color.NEGRO, strategy -> strategy.getCaballoNegroMoveGenerator()),
 	
-	ALFIL_BLANCO(Color.BLANCO, (MoveGeneratorStrategy strategy) -> strategy.getAlfilBlancoMoveGenerator()),
-	ALFIL_NEGRO(Color.NEGRO, (MoveGeneratorStrategy strategy) -> strategy.getAlfilNegroMoveGenerator()),
+	ALFIL_BLANCO(Color.BLANCO, strategy -> strategy.getAlfilBlancoMoveGenerator()),
+	ALFIL_NEGRO(Color.NEGRO, strategy -> strategy.getAlfilNegroMoveGenerator()),
 	
-	REINA_BLANCO(Color.BLANCO, (MoveGeneratorStrategy strategy) -> strategy.getReinaBlancaMoveGenerator()),
-	REINA_NEGRO(Color.NEGRO, (MoveGeneratorStrategy strategy) -> strategy.getReinaNegraMoveGenerator()),
+	REINA_BLANCO(Color.BLANCO, strategy -> strategy.getReinaBlancaMoveGenerator()),
+	REINA_NEGRO(Color.NEGRO, strategy -> strategy.getReinaNegraMoveGenerator()),
 	
-	REY_BLANCO(Color.BLANCO, (MoveGeneratorStrategy strategy) -> strategy.getReyBlancoMoveGenerator()),
-	REY_NEGRO(Color.NEGRO, (MoveGeneratorStrategy strategy) -> strategy.getReyNegroMoveGenerator());
+	REY_BLANCO(Color.BLANCO, strategy -> strategy.getReyBlancoMoveGenerator()),
+	REY_NEGRO(Color.NEGRO, strategy -> strategy.getReyNegroMoveGenerator());
 	
 	private Color color;
-	private StrategySelector selector = null;
+	private Function<MoveGeneratorStrategy, MoveGenerator> selector = null;
 	
-	private Pieza(Color color, StrategySelector selector) {
+	private Pieza(Color color, Function<MoveGeneratorStrategy, MoveGenerator> selector) {
 		this.color = color;
 		this.selector = selector;
 	}
-	
-	public Color getColor(){
+
+	public Color getColor() {
 		return color;
 	}
+
+	public MoveGenerator getMoveGenerator(MoveGeneratorStrategy strategy) {
+		return selector.apply(strategy);
+	}	
 	
 	public static Pieza getRey(Color color){
 		switch (color) {
@@ -78,7 +84,4 @@ public enum Pieza {
 		}
 	}	
 
-	public MoveGenerator getMoveGenerator(MoveGeneratorStrategy strategy) {
-		return this.selector.getMoveGenerator(strategy);
-	}
 }
--- src/main/java/chess/StrategySelector.java
+++
@@ -1,9 +0,0 @@
-package chess;
-
-import movegenerators.MoveGenerator;
-import movegenerators.MoveGeneratorStrategy;
-
-@FunctionalInterface
-public interface StrategySelector {
-	MoveGenerator getMoveGenerator(MoveGeneratorStrategy strategy);
-}
