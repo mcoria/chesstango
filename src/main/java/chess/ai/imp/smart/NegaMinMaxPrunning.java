59129121cc4a8f448c06e4d61f7e34dc8619cbb9
Mauricio Coria <coriamauricio@gmail.com>
2022-06-02 00:43:07 -0300

    Implementing NegaMinMaxPrunning

---
+++ src/main/java/chess/ai/imp/smart/NegaMinMaxPrunning.java
@@ -0,0 +1,101 @@
+/**
+ *
+ */
+package chess.ai.imp.smart;
+
+import chess.board.Color;
+import chess.board.Game;
+import chess.board.moves.Move;
+import chess.board.moves.containers.MoveContainerReader;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Mauricio Coria
+ */
+public class NegaMinMaxPrunning extends AbstractSmart {
+    private static final int DEFAULT_MAXLEVEL = 7;
+    private final int maxLevel;
+    private final GameEvaluator evaluator = new GameEvaluator();
+    private Game game = null;
+    private boolean keepProcessing;
+
+    public NegaMinMaxPrunning() {
+        this(DEFAULT_MAXLEVEL);
+    }
+
+    public NegaMinMaxPrunning(int level) {
+        this.maxLevel = level;
+    }
+
+    @Override
+    public Move findBestMove(final Game game) {
+        this.game = game;
+        this.keepProcessing = true;
+
+        final List<Move> possibleMoves = new ArrayList<Move>();
+
+
+        int alpha =  GameEvaluator.INFINITE_NEGATIVE;
+        final int beta = GameEvaluator.INFINITE_POSITIVE;
+        int maxValue = GameEvaluator.INFINITE_NEGATIVE;
+
+        boolean search = true;
+        Iterator<Move> possibleMovesIterator = game.getPossibleMoves().iterator();
+        while (possibleMovesIterator.hasNext() && search && keepProcessing) {
+            Move move = possibleMovesIterator.next();
+            game.executeMove(move);
+
+            maxValue = Math.max(maxValue, - negaMinMax(maxLevel - 1, -beta, -alpha));
+
+            if (maxValue > alpha) {
+                alpha = maxValue;
+                possibleMoves.clear();
+                if (maxValue == GameEvaluator.INFINITE_POSITIVE) {
+                    search = false;
+                }
+            }
+
+            if (alpha == maxValue) {
+                possibleMoves.add(move);
+            }
+
+            game.undoMove();
+        }
+        return selectMove(possibleMoves);
+    }
+
+    private Integer negaMinMax(final int currentLevel, int alpha, final int beta) {
+        MoveContainerReader possibleMoves = game.getPossibleMoves();
+        if (currentLevel == 0 || possibleMoves.size() == 0) {
+            return evaluator.evaluate(game);
+        } else {
+            boolean search = true;
+            int maxValue = GameEvaluator.INFINITE_NEGATIVE;
+            Iterator<Move> possibleMovesIterator = possibleMoves.iterator();
+            while (possibleMovesIterator.hasNext() && search && keepProcessing) {
+                Move move = possibleMovesIterator.next();
+                game.executeMove(move);
+
+                maxValue = Math.max(maxValue, - negaMinMax(currentLevel - 1, -beta, -alpha));
+
+                alpha = Math.max(maxValue, alpha);
+
+                if (alpha >= beta) {
+                    search = false;
+                }
+
+                game.undoMove();
+            }
+            return maxValue;
+        }
+    }
+
+    @Override
+    public void stopProcessing() {
+        keepProcessing = false;
+    }
+
+}
--- src/test/java/chess/ai/imp/smart/MateInTestAbstract.java
+++ src/test/java/chess/ai/imp/smart/MateInTestAbstract.java
@@ -22,7 +22,8 @@ public abstract class MateInTestAbstract {
 	public void setUp() {
 		//bestMoveFinder = new MinMaxPrunning(getMaxLevel());
 		//bestMoveFinder = new MinMax(getMaxLevel());
-		bestMoveFinder = new NegaMinMax(getMaxLevel());
+		//bestMoveFinder = new NegaMinMax(getMaxLevel());
+		bestMoveFinder = new NegaMinMaxPrunning(getMaxLevel());
 	}
 
 	protected Game getGame(String string) {
