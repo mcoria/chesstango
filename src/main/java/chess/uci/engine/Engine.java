a91fd4c020b2814c6221b65449d0d50a81cdadc5
Mauricio Coria <coriamauricio@gmail.com>
2022-06-02 00:57:15 -0300

    Engine with NegaMinMax level 5

--- src/main/java/chess/uci/engine/Engine.java
+++ src/main/java/chess/uci/engine/Engine.java
@@ -9,6 +9,7 @@ import java.util.concurrent.Executors;
 
 import chess.ai.BestMoveFinder;
 import chess.ai.imp.smart.MinMaxPrunning;
+import chess.ai.imp.smart.NegaMinMaxPrunning;
 import chess.board.Game;
 import chess.board.builder.imp.GameBuilder;
 import chess.board.representations.MoveEncoder;
@@ -38,7 +39,8 @@ public class Engine {
 	public Engine(UCIResponseChannel responseChannel) {
 		this.responseChannel = responseChannel;
 		//this.bestMoveFinder = new MinMax();
-		this.bestMoveFinder = new MinMaxPrunning();
+		//this.bestMoveFinder = new MinMaxPrunning();
+		this.bestMoveFinder = new NegaMinMaxPrunning(5);
 		this.moveEncoder = new MoveEncoder();
 	}
 
