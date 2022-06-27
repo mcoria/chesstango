b845bfbd0edbafd9c3e0ebb3322c0cd55a9f6836
Mauricio Coria <coriamauricio@gmail.com>
2022-06-27 09:40:45 -0300

    Adjusting GameEvaluator constants

--- src/main/java/net/chesstango/ai/imp/smart/evaluation/GameEvaluator.java
+++ src/main/java/net/chesstango/ai/imp/smart/evaluation/GameEvaluator.java
@@ -14,9 +14,44 @@ import java.util.Set;
  */
 public class GameEvaluator  {
 
-    private static final int FACTOR_MATERIAL_DEFAULT = 365;
-    private static final int FACTOR_EXPANSION_DEFAULT = 325;
-    private static final int FACTOR_ATAQUE_DEFAULT = 91;
+    //TOOD: Deberiamos codificar los siguientes genes para una poblacion inicial
+    /*
+        Para esta lista de juegos vs spike:
+        (FENDecoder.INITIAL_FEN, "4rr1k/pppb2bp/2q1n1p1/4p3/8/1BPPBN2/PP2QPP1/2KR3R w - - 8 20", "r1bqkb1r/pp3ppp/2nppn2/1N6/2P1P3/2N5/PP3PPP/R1BQKB1R b KQkq - 2 7", "rn1qkbnr/pp2ppp1/2p4p/3pPb2/3P2PP/8/PPP2P2/RNBQKBNR b KQkq g3 0 5"));
+        Depth 4 vs Depth 1
+        Evaluacion con gene1=[833] gene2=[352] gene3=[91] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[145] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[169] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[46] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[170] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[118] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[69] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[172] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[356] gene3=[91] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[129] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[355] gene3=[91] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[50] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[83] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[164] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[56] ; puntos = [9]
+        Evaluacion con gene1=[838] gene2=[352] gene3=[66] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[102] ; puntos = [9]
+        Evaluacion con gene1=[833] gene2=[352] gene3=[82] ; puntos = [9]
+        Evaluacion con gene1=[825] gene2=[352] gene3=[91] ; puntos = [9]
+        Evaluacion con gene1=[837] gene2=[352] gene3=[52] ; puntos = [9]
+
+        Depth 3 vs Depth 1
+        Evaluacion con gene1=[320] gene2=[268] gene3=[991] ; puntos = [7]
+        Evaluacion con gene1=[320] gene2=[268] gene3=[969] ; puntos = [7]
+        Evaluacion con gene1=[320] gene2=[268] gene3=[960] ; puntos = [7]
+        Evaluacion con gene1=[320] gene2=[259] gene3=[991] ; puntos = [7]
+     */
+
+
+
+    private static final int FACTOR_MATERIAL_DEFAULT = 320;
+    private static final int FACTOR_EXPANSION_DEFAULT = 268;
+    private static final int FACTOR_ATAQUE_DEFAULT = 991;
 
     public static final int INFINITE_POSITIVE = Integer.MAX_VALUE;
     public static final int INFINITE_NEGATIVE = -INFINITE_POSITIVE;
--- src/test/java/net/chesstango/arena/ChessEvolutionTest.java
+++ src/test/java/net/chesstango/arena/ChessEvolutionTest.java
@@ -45,33 +45,6 @@ public class ChessEvolutionTest {
     }
 
 
-    //TOOD: Deberiamos codificar los siguientes genes para una poblacion inicial
-/*
-    Para esta lista de juegos:
-    (FENDecoder.INITIAL_FEN, "4rr1k/pppb2bp/2q1n1p1/4p3/8/1BPPBN2/PP2QPP1/2KR3R w - - 8 20", "r1bqkb1r/pp3ppp/2nppn2/1N6/2P1P3/2N5/PP3PPP/R1BQKB1R b KQkq - 2 7", "rn1qkbnr/pp2ppp1/2p4p/3pPb2/3P2PP/8/PPP2P2/RNBQKBNR b KQkq g3 0 5"));
-    Evaluacion con gene1=[833] gene2=[352] gene3=[91] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[145] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[169] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[46] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[170] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[118] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[69] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[172] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[356] gene3=[91] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[129] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[355] gene3=[91] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[50] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[83] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[164] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[56] ; puntos = [9]
-    Evaluacion con gene1=[838] gene2=[352] gene3=[66] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[102] ; puntos = [9]
-    Evaluacion con gene1=[833] gene2=[352] gene3=[82] ; puntos = [9]
-    Evaluacion con gene1=[825] gene2=[352] gene3=[91] ; puntos = [9]
-    Evaluacion con gene1=[837] gene2=[352] gene3=[52] ; puntos = [9]
-*/
-
-
     @Test
     public void evolveEvaluationFunction() {
         // 1.) Define the genotype (factory) suitable for the problem.
