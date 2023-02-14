6fccebda57ab769c62bb0a0de00ce3f5d29ae2ca
Mauricio Coria <coriamauricio@gmail.com>
2023-02-14 10:33:52 -0300

    Removing requieres transitive

--- uci-arena/src/main/java/module-info.java
+++ uci-arena/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module net.chesstango.uci.arena {
     requires net.chesstango.uci;
     requires net.chesstango.uci.engine;
     requires net.chesstango.evaluation;
+    requires net.chesstango.board;
 }
--- uci-protocol/src/main/java/module-info.java
+++ uci-protocol/src/main/java/module-info.java
@@ -4,5 +4,5 @@ module net.chesstango.uci {
     exports net.chesstango.uci.protocol.responses;
     exports net.chesstango.uci.protocol.stream;
     exports net.chesstango.uci.protocol.stream.strings;
-    requires transitive net.chesstango.board;
+    requires net.chesstango.board;
 }
