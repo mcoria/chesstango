93fd674ac5202388dbee57d1eddc95a0191d0217
Mauricio Coria <coriamauricio@gmail.com>
2023-03-12 09:39:25 -0300

    Capturing search metrics

--- uci-arena/src/main/java/net/chesstango/uci/arena/MatchMain.java
+++ uci-arena/src/main/java/net/chesstango/uci/arena/MatchMain.java
@@ -1,16 +1,15 @@
 package net.chesstango.uci.arena;
 
-import net.chesstango.board.representations.Transcoding;
 import net.chesstango.evaluation.GameEvaluator;
-import net.chesstango.evaluation.imp.*;
+import net.chesstango.evaluation.imp.GameEvaluatorSEandImp02;
 import net.chesstango.search.DefaultSearchMove;
 import net.chesstango.search.SearchMove;
 import net.chesstango.uci.arena.reports.Reports;
 import net.chesstango.uci.arena.reports.SessionReport;
+import net.chesstango.uci.engine.EngineTango;
 import net.chesstango.uci.gui.EngineController;
 import net.chesstango.uci.gui.EngineControllerImp;
 import net.chesstango.uci.proxy.EngineProxy;
-import net.chesstango.uci.engine.EngineTango;
 import net.chesstango.uci.proxy.ProxyConfig;
 
 import java.lang.reflect.InvocationTargetException;
@@ -35,20 +34,20 @@ public class MatchMain {
             "r1b1kb1r/pp1n1pp1/2p1pq1p/3p4/2PP4/2N1PN2/PP3PPP/R2QKB1R w KQkq - 1 8",
             "r1bqk2r/pp1n1ppp/2pbpn2/3p4/2PP4/2N1PN2/PPQ2PPP/R1B1KB1R w KQkq - 4 7",
             "rn1qk2r/p1pp1ppp/bp2pn2/8/1bPP4/1P3NP1/P2BPP1P/RN1QKB1R b KQkq - 2 6");
-    private static final int DEPTH = 4;
+    private static final int DEPTH = 1;
     private static final boolean MATCH_DEBUG = false;
 
     public static void main(String[] args) {
         EngineController engineController1 = createTangoController(GameEvaluatorSEandImp02.class);
 
-        //EngineController engineController2 = new EngineControllerImp(new EngineProxy(ProxyConfig.loadEngineConfig("GameEvaluatorImp02")).setLogging(false));
-        EngineController engineController2 = createTangoController(GameEvaluatorImp02.class);
+        //EngineController engineController2 = new EngineControllerImp(new EngineProxy(ProxyConfig.loadEngineConfig("GameEvaluatorImp02")).setLogging(false)).overrideEngineName("GameEvaluatorImp02");
+        //EngineController engineController2 = createTangoController(GameEvaluatorImp02.class);
+        EngineController engineController2 = new EngineControllerImp(new EngineProxy(ProxyConfig.loadEngineConfig("Spike")).setLogging(false));
 
         List<GameResult> matchResult = new MatchMain().play(engineController1, engineController2);
 
-        // Solo para propositos de ordenar la tabla de salida se especifican los engines
+        // Solo para ordenar la tabla de salida se especifican los engines en la lista
         new Reports().printEngineControllersReport(Arrays.asList(engineController1, engineController2), matchResult);
-
         new SessionReport().printTangoStatics(Arrays.asList(engineController1, engineController2), matchResult);
     }
 
@@ -69,8 +68,8 @@ public class MatchMain {
 
     private static List<String> getFenList() {
         //return new Transcoding().pgnFileToFenPositions(MatchMain.class.getClassLoader().getResourceAsStream("Balsa_Top50.pgn"));
-        //return GAMES_BALSA_TOP10;
-        return Arrays.asList("r1bqk2r/pp1n1ppp/2pbpn2/3p4/2PP4/2N1PN2/PPQ2PPP/R1B1KB1R w KQkq - 4 7");
+        //return Arrays.asList("rnbqkb1r/5ppp/p2ppn2/1p6/3NP3/2N1BP2/PPP3PP/R2QKB1R w KQkq b6 0 8");
+        return GAMES_BALSA_TOP10;
     }
 
     private static EngineController createTangoController(Class<? extends GameEvaluator> gameEvaluatorClass) {
