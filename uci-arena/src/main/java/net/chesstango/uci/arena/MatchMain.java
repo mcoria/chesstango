182ecfb6063e7ae570f60f9f7b4b9476f91c6246
Mauricio Coria <coriamauricio@gmail.com>
2023-06-01 09:13:42 -0300

    Adding MadChess proxy

--- uci-arena/src/main/java/net/chesstango/uci/arena/MatchMain.java
+++ uci-arena/src/main/java/net/chesstango/uci/arena/MatchMain.java
@@ -24,7 +24,7 @@ import java.util.List;
  */
 public class MatchMain implements MatchListener {
 
-    private static final CmdGo CMD_GO = new CmdGo().setGoType(CmdGo.GoType.DEPTH).setDepth(4);
+    private static final CmdGo CMD_GO = new CmdGo().setGoType(CmdGo.GoType.DEPTH).setDepth(5);
     //private static final CmdGo CMD_GO = new CmdGo().setGoType(CmdGo.GoType.MOVE_TIME).setTimeOut(500);
 
     private static final boolean MATCH_DEBUG = false;
