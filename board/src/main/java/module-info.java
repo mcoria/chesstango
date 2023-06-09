2772a9ed7f6792f56bf82fae54d79a75a4bb4a8d
Mauricio Coria <coriamauricio@gmail.com>
2023-06-09 00:11:19 -0300

    Deleting old move implementation

--- board/src/main/java/module-info.java
+++ board/src/main/java/module-info.java
@@ -18,5 +18,4 @@ module net.chesstango.board {
     exports net.chesstango.board.movesgenerators.legal;
     exports net.chesstango.board.position.imp;
     exports net.chesstango.board.perft;
-    exports net.chesstango.board.moves.impl.inheritance;
 }
