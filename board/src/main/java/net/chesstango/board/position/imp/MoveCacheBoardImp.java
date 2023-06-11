03843fb20a81cbeab437cec1a3b1c51a0c4e2bcf
Mauricio Coria <coriamauricio@gmail.com>
2023-06-11 01:41:35 -0300

    Refactoring MoveCacheBoardImp

--- board/src/main/java/net/chesstango/board/position/imp/MoveCacheBoardImp.java
+++ board/src/main/java/net/chesstango/board/position/imp/MoveCacheBoardImp.java
@@ -5,50 +5,48 @@ import net.chesstango.board.movesgenerators.pseudo.MoveGeneratorResult;
 import net.chesstango.board.position.MoveCacheBoard;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * @author Mauricio Coria
- *
  * TODO: LOS TESTS DE MOVIMIENTOS DEBERIAN PROBAR LOS AFFECTS
- *
  */
 public class MoveCacheBoardImp implements MoveCacheBoard {
-	
-	protected MoveGeneratorResult[] pseudoMoves = new MoveGeneratorResult[64];
-	protected long[] affects = new long[64];
 
-	private List<MoveGeneratorResult> currentClearedSquares = new ArrayList<MoveGeneratorResult>();
-	private final Deque<List<MoveGeneratorResult>> clearedSquares = new ArrayDeque<List<MoveGeneratorResult>>();
-	
-	@Override
-	public MoveGeneratorResult getPseudoMovesResult(Square key) {
-		return  pseudoMoves[key.toIdx()];
-	}	
-	 
-	@Override
-	public void setPseudoMoves(Square key, MoveGeneratorResult generatorResult) {
-		pseudoMoves[key.toIdx()] = generatorResult;
-		long keyAdded = key.getBitPosition();
-		long affectedByCollection = generatorResult.getAffectedByPositions();
+    protected MoveGeneratorResult[] pseudoMoves = new MoveGeneratorResult[64];
+    protected long[] affects = new long[64];
 
-		
-		while(affectedByCollection != 0){
-			long posicionLng = Long.lowestOneBit(affectedByCollection);
-			int idx = Long.numberOfTrailingZeros(posicionLng);
-			affects[idx] |= keyAdded;
-			affectedByCollection &= ~posicionLng;
-		}		
-	}		
+	private final Deque<MoveGeneratorResult> clearedSquares = new ArrayDeque<>();
+    private int currentClearedSquaresCounter = 0;
 
-	@Override
+	private final Deque<Integer> clearedSquaresCounters = new ArrayDeque<>();
+    @Override
+    public MoveGeneratorResult getPseudoMovesResult(Square key) {
+        return pseudoMoves[key.toIdx()];
+    }
+
+    @Override
+    public void setPseudoMoves(Square key, MoveGeneratorResult generatorResult) {
+        pseudoMoves[key.toIdx()] = generatorResult;
+        final long keyAdded = key.getBitPosition();
+
+        long affectedByCollection = generatorResult.getAffectedByPositions();
+        while (affectedByCollection != 0) {
+            long posicionLng = Long.lowestOneBit(affectedByCollection);
+            int idx = Long.numberOfTrailingZeros(posicionLng);
+            affects[idx] |= keyAdded;
+            affectedByCollection &= ~posicionLng;
+        }
+    }
+
+    @Override
 	public void clearPseudoMoves(Square key, boolean trackCleared) {
 		clearPseudoMoves(affects[key.toIdx()] | (pseudoMoves[key.toIdx()] != null ? key.getBitPosition() : 0), trackCleared);
 	}
 
-	@Override
+    @Override
 	public void clearPseudoMoves(Square key1, Square key2, boolean trackCleared) {
 		clearPseudoMoves(affects[key1.toIdx()] | (pseudoMoves[key1.toIdx()] != null ? key1.getBitPosition() : 0)
 				                  | affects[key2.toIdx()] | (pseudoMoves[key2.toIdx()] != null ? key2.getBitPosition() : 0), trackCleared);
@@ -69,56 +67,64 @@ public class MoveCacheBoardImp implements MoveCacheBoard {
 				                 | affects[key4.toIdx()] | (pseudoMoves[key4.toIdx()] != null ? key4.getBitPosition() : 0), trackCleared);
 	}
 
-	@Override
-	public void clearPseudoMoves(long clearSquares, boolean trackCleared) {
-		long affectsBySquares = 0;
-		for(int i = 0; i < 64; i++){
-			if( (clearSquares & (1L << i))  != 0 ) {
-				MoveGeneratorResult pseudoMove = pseudoMoves[i];
-				if(pseudoMove != null){
-					affectsBySquares |= pseudoMove.getAffectedByPositions();
-					if(trackCleared){
-						currentClearedSquares.add(pseudoMove);
-					}
-					pseudoMoves[i] = null;
-				}
-			}
-		}
+    @Override
+    public void clearPseudoMoves(final long clearSquares, final boolean trackCleared) {
+        final long keyRemoved = ~clearSquares;
 
-		long keyRemoved = ~clearSquares;
-		for(int i = 0; i < 64; i++){
-			if( (affectsBySquares & (1L << i))  != 0 ) {
-				affects[i] &= keyRemoved;
-			}
-		}
-	}
+        long affectsBySquares = 0;
+
+        long positions = clearSquares;
+        while(positions != 0){
+            long posicionLng = Long.lowestOneBit(positions);
+            int i = Long.numberOfTrailingZeros(posicionLng);
+            MoveGeneratorResult pseudoMove = pseudoMoves[i];
+            if (pseudoMove != null) {
+                affectsBySquares |= pseudoMove.getAffectedByPositions();
+                if(trackCleared){
+                    clearedSquares.push(pseudoMove);
+                    currentClearedSquaresCounter++;
+                }
+                pseudoMoves[i] = null;
+            }
+            positions &= ~posicionLng;
+        }
+
+        while(affectsBySquares != 0) {
+            long posicionLng = Long.lowestOneBit(affectsBySquares);
+            int i = Long.numberOfTrailingZeros(posicionLng);
+            affects[i] &= keyRemoved;
+            affectsBySquares &= ~posicionLng;
+        }
+    }
 
 
 	@Override
 	public void pushCleared() {
-		clearedSquares.push(currentClearedSquares);
-		currentClearedSquares = new ArrayList<MoveGeneratorResult>();
+        clearedSquaresCounters.push(currentClearedSquaresCounter);
+        currentClearedSquaresCounter = 0;
 	}
 
 	//TODO: este metodo consume el 20% del procesamiento
 	@Override
 	public void popCleared() {
-		currentClearedSquares.stream()
-				.forEach(generatorResult -> setPseudoMoves(generatorResult.getFrom().getSquare(), generatorResult));
-		
-		currentClearedSquares = clearedSquares.pop();
+        for(int i = 0; i < currentClearedSquaresCounter; i++){
+            MoveGeneratorResult generatorResult = clearedSquares.pop();
+            setPseudoMoves(generatorResult.getFrom().getSquare(), generatorResult);
+
+        }
+        currentClearedSquaresCounter = clearedSquaresCounters.pop();
 	}	
 
 	@Override
-	public String toString() {
-		StringBuffer buffer = new StringBuffer(); 
-		for (MoveGeneratorResult result : pseudoMoves) {
-			if(result != null){
-				buffer.append(result + "\n");
-			}
-		}
-		
-		return buffer.toString();
-	}
-	
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        for (MoveGeneratorResult result : pseudoMoves) {
+            if (result != null) {
+                buffer.append(result + "\n");
+            }
+        }
+
+        return buffer.toString();
+    }
+
 }
