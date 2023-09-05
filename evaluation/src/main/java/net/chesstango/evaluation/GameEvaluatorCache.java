a748f1f2e024474b2292fc7edc17780213ff882b
Mauricio Coria <coriamauricio@gmail.com>
2023-09-05 09:40:35 -0300

    CHT-79: Agregar cache de evaluaciones

---
+++ evaluation/src/main/java/net/chesstango/evaluation/GameEvaluatorCache.java
@@ -0,0 +1,54 @@
+package net.chesstango.evaluation;
+
+import net.chesstango.board.Game;
+
+/**
+ * @author Mauricio Coria
+ */
+public class GameEvaluatorCache implements GameEvaluator {
+
+    private final GameEvaluator imp;
+    private final GameEvaluatorCacheEntry[] cache;
+
+    private long statisticCounter = 0;
+
+    private static final int ARRAY_SIZE = 1024 * 512;
+
+    public GameEvaluatorCache(GameEvaluator imp) {
+        this.imp = imp;
+        this.cache = new GameEvaluatorCacheEntry[ARRAY_SIZE];
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            this.cache[i] = new GameEvaluatorCacheEntry();
+        }
+    }
+
+    @Override
+    public int evaluate(Game game) {
+        long hash = game.getChessPosition().getZobristHash();
+
+        int idx = (int) Math.abs(hash % ARRAY_SIZE);
+
+        GameEvaluatorCacheEntry entry = cache[idx];
+
+        if (entry.hash != hash) {
+            statisticCounter++;
+            entry.hash = hash;
+            entry.evaluation = imp.evaluate(game);
+        }
+
+        return entry.evaluation;
+    }
+
+    public void resetStatisticCounter() {
+        statisticCounter = 0;
+    }
+
+    public long getStatisticCounter() {
+        return statisticCounter;
+    }
+
+    private static class GameEvaluatorCacheEntry {
+        long hash;
+        int evaluation;
+    }
+}
---
+++ search/src/main/java/net/chesstango/search/smart/alphabeta/filters/EvaluatorCacheStatistics.java
@@ -0,0 +1,73 @@
+package net.chesstango.search.smart.alphabeta.filters;
+
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import net.chesstango.board.Game;
+import net.chesstango.evaluation.GameEvaluator;
+import net.chesstango.evaluation.GameEvaluatorCache;
+import net.chesstango.search.SearchMoveResult;
+import net.chesstango.search.smart.SearchContext;
+import net.chesstango.search.smart.SearchLifeCycle;
+import net.chesstango.search.smart.statistics.EvaluationEntry;
+import net.chesstango.search.smart.statistics.EvaluationStatistics;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @author Mauricio Coria
+ */
+public class EvaluatorCacheStatistics implements GameEvaluator, SearchLifeCycle {
+    private GameEvaluatorCache gameEvaluatorCache;
+    private Set<EvaluationEntry> evaluations;
+
+    @Setter
+    @Accessors(chain = true)
+    private boolean trackEvaluations;
+
+
+    public EvaluatorCacheStatistics(GameEvaluatorCache instance) {
+        this.gameEvaluatorCache = instance;
+    }
+
+    @Override
+    public int evaluate(Game game) {
+        int evaluation = gameEvaluatorCache.evaluate(game);
+        if (trackEvaluations) {
+            evaluations.add(new EvaluationEntry(game.getChessPosition().getZobristHash(), evaluation));
+        }
+        return evaluation;
+    }
+
+    @Override
+    public void beforeSearch(Game game, int maxDepth) {
+        gameEvaluatorCache.resetStatisticCounter();
+        if (trackEvaluations) {
+            evaluations = new LinkedHashSet<>();
+        }
+    }
+
+    @Override
+    public void afterSearch(SearchMoveResult result) {
+        if (result != null) {
+            result.setEvaluationStatistics(new EvaluationStatistics(gameEvaluatorCache.getStatisticCounter(), evaluations));
+        }
+    }
+
+    @Override
+    public void beforeSearchByDepth(SearchContext context) {
+    }
+
+    @Override
+    public void afterSearchByDepth(SearchMoveResult result) {
+    }
+
+    @Override
+    public void stopSearching() {
+    }
+
+    @Override
+    public void reset() {
+    }
+
+}
