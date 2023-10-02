2b8f1e4e4365fdd34223fb4c6b7cb0edb7b27c43
Mauricio Coria <coriamauricio@gmail.com>
2023-10-02 10:51:01 -0300

    CHT-90: extendting AlphaBetaStatisticsTest

--- search/src/test/java/net/chesstango/search/smart/alphabeta/AlphaBetaStatisticsTest.java
+++ search/src/test/java/net/chesstango/search/smart/alphabeta/AlphaBetaStatisticsTest.java
@@ -5,21 +5,29 @@ import net.chesstango.board.moves.Move;
 import net.chesstango.board.representations.fen.FENDecoder;
 import net.chesstango.evaluation.GameEvaluator;
 import net.chesstango.evaluation.evaluators.EvaluatorByMaterial;
+import net.chesstango.search.SearchMove;
 import net.chesstango.search.SearchMoveResult;
+import net.chesstango.search.builders.AlphaBetaBuilder;
+import net.chesstango.search.reports.EvaluationReport;
+import net.chesstango.search.reports.NodesReport;
+import net.chesstango.search.reports.NodesReportModel;
 import net.chesstango.search.smart.NoIterativeDeepening;
 import net.chesstango.search.smart.alphabeta.filters.*;
 import net.chesstango.search.smart.alphabeta.listeners.SetBestMoves;
 import net.chesstango.search.smart.alphabeta.listeners.SetTranspositionTables;
 import net.chesstango.search.smart.sorters.DefaultMoveSorter;
 import net.chesstango.search.smart.sorters.MoveSorter;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 /**
@@ -27,52 +35,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class AlphaBetaStatisticsTest {
 
-    private AlphaBetaFacade alphaBetaFacade;
+    private SearchMoveResult searchResult;
 
-    @BeforeEach
-    public void setup() {
-        MoveSorter moveSorter = new DefaultMoveSorter();
+    private static final boolean PRINT_REPORT = true;
 
-        GameEvaluator gameEvaluator = new EvaluatorByMaterial();
+    @AfterEach
+    public void printReport() {
+        if (PRINT_REPORT) {
+            new NodesReport()
+                    .withMoveResults(List.of(searchResult))
+                    .withCutoffStatistics()
+                    .withNodesVisitedStatistics()
+                    .printReport(System.out);
 
-        AlphaBetaStatistics alphaBetaStatistics = new AlphaBetaStatistics();
-        TranspositionTable transpositionTable = new TranspositionTable();
-        AlphaBeta alphaBeta = new AlphaBeta();
-        AlphaBetaFlowControl alphaBetaFlowControl = new AlphaBetaFlowControl();
-        QuiescenceNull quiescence = new QuiescenceNull();
+            new EvaluationReport()
+                    .withMoveResults(List.of(searchResult))
+                    //.withExportEvaluations()
+                    .withEvaluationsStatistics()
+                    .printReport(System.out);
 
-        alphaBetaStatistics.setNext(transpositionTable);
-
-        transpositionTable.setNext(alphaBetaFlowControl);
-
-        alphaBetaFlowControl.setNext(alphaBeta);
-        alphaBetaFlowControl.setQuiescence(quiescence);
-        alphaBetaFlowControl.setGameEvaluator(gameEvaluator);
-
-        alphaBeta.setNext(alphaBetaStatistics);
-        alphaBeta.setMoveSorter(moveSorter);
-
-        quiescence.setGameEvaluator(gameEvaluator);
-
-        this.alphaBetaFacade = new AlphaBetaFacade();
-        this.alphaBetaFacade.setAlphaBetaSearch(alphaBetaStatistics);
-        this.alphaBetaFacade.setSearchActions(List.of(
-                new SetTranspositionTables(),
-                alphaBetaStatistics,
-                transpositionTable,
-                alphaBetaFlowControl,
-                alphaBeta,
-                quiescence,
-                moveSorter,
-                new SetBestMoves()));
+            /*
+            new PrincipalVariationReport()
+                    .withMoveResults(List.of(searchResult))
+                    .printReport(System.out);
+            */
+        }
     }
 
     @Test
     @Disabled
     public void testDistinctMoves() {
+        SearchMove moveFinder = new AlphaBetaBuilder()
+                .withGameEvaluator(new EvaluatorByMaterial())
+                .withTranspositionTable()
+                .withStatistics()
+                .build();
+
         Game game = FENDecoder.loadGame(FENDecoder.INITIAL_FEN);
 
-        SearchMoveResult searchResult = new NoIterativeDeepening(alphaBetaFacade).search(game, 2);
+        SearchMoveResult searchResult = moveFinder.search(game, 2);
 
         Set<Move>[] distinctMoves = null; //searchResult.getDistinctMovesPerLevel();
 
@@ -82,9 +83,15 @@ public class AlphaBetaStatisticsTest {
 
     @Test
     public void testVisitedNodesCounters() {
+        SearchMove moveFinder = new AlphaBetaBuilder()
+                .withGameEvaluator(new EvaluatorByMaterial())
+                .withTranspositionTable()
+                .withStatistics()
+                .build();
+
         Game game = FENDecoder.loadGame(FENDecoder.INITIAL_FEN);
 
-        SearchMoveResult searchResult = new NoIterativeDeepening(alphaBetaFacade).search(game, 2);
+        searchResult = moveFinder.search(game, 2);
 
         int[] visitedNodesCounters = searchResult.getRegularNodeStatistics().visitedNodesCounters();
 
@@ -103,9 +110,15 @@ public class AlphaBetaStatisticsTest {
 
     @Test
     public void testExpectedNodesCounters() {
+        SearchMove moveFinder = new AlphaBetaBuilder()
+                .withGameEvaluator(new EvaluatorByMaterial())
+                .withTranspositionTable()
+                .withStatistics()
+                .build();
+
         Game game = FENDecoder.loadGame(FENDecoder.INITIAL_FEN);
 
-        SearchMoveResult searchResult = new NoIterativeDeepening(alphaBetaFacade).search(game, 2);
+        searchResult = moveFinder.search(game, 2);
 
         int[] visitedNodesCounters = searchResult.getRegularNodeStatistics().expectedNodesCounters();
 
@@ -115,10 +128,120 @@ public class AlphaBetaStatisticsTest {
 
     @Test
     public void testEvaluationCollisions() {
+        SearchMove moveFinder = new AlphaBetaBuilder()
+                .withGameEvaluator(new EvaluatorByMaterial())
+                .withTranspositionTable()
+                .withStatistics()
+                .build();
+
         Game game = FENDecoder.loadGame(FENDecoder.INITIAL_FEN);
 
-        SearchMoveResult searchResult = new NoIterativeDeepening(alphaBetaFacade).search(game, 2);
+        searchResult = moveFinder.search(game, 2);
 
         assertEquals(20, searchResult.getBestMovesCounter());
+    }
+
+    @Test
+    public void testSearch_01() {
+        SearchMove moveFinder = new AlphaBetaBuilder()
+                .withGameEvaluator(new EvaluatorByMaterial())
+                .withStatistics()
+                .build();
+
+        Game game = FENDecoder.loadGame(FENDecoder.INITIAL_FEN);
+
+        searchResult = moveFinder.search(game, 1);
+
+        int[] visitedNodesCounters = searchResult.getRegularNodeStatistics().visitedNodesCounters();
+        int visitedNodesCountersTotal = Arrays.stream(searchResult.getRegularNodeStatistics().visitedNodesCounters()).sum();
+        int executedMoves = searchResult.getExecutedMoves();
+
+        assertEquals(20, visitedNodesCounters[0]);
+        assertEquals(20, visitedNodesCountersTotal);
+        assertNull(searchResult.getQuiescenceNodeStatistics());
+        assertEquals(20, executedMoves);
+    }
+
+    @Test
+    public void testSearch_02() {
+        SearchMove moveFinder = new AlphaBetaBuilder()
+                .withGameEvaluator(new EvaluatorByMaterial())
+                .withTranspositionTable()
+                .withStatistics()
+                .build();
+
+        Game game = FENDecoder.loadGame(FENDecoder.INITIAL_FEN);
+
+        searchResult = moveFinder.search(game, 1);
+
+        int[] visitedNodesCounters = searchResult.getRegularNodeStatistics().visitedNodesCounters();
+        int visitedNodesCountersTotal = Arrays.stream(searchResult.getRegularNodeStatistics().visitedNodesCounters()).sum();
+        int executedMoves = searchResult.getExecutedMoves();
+
+        assertEquals(20, visitedNodesCounters[0]);
+        assertEquals(20, visitedNodesCountersTotal);
+        assertNull(searchResult.getQuiescenceNodeStatistics());
+        /**
+         * Los 61 movimientos se descomponen en:
+         * 20 movimientos de la busqueda regular AlphaBeta
+         *  1 movimiento  de SetPrincipalVariation
+         * 20 movimientos de SetMoveEvaluations
+         * 20 movimientos de SetBestMoves
+         */
+        assertEquals(61, executedMoves);
+
+    }
+
+    @Test
+    public void testSearch_03() {
+        SearchMove moveFinder = new AlphaBetaBuilder()
+                .withGameEvaluator(new EvaluatorByMaterial())
+                .withQuiescence()
+                .withStatistics()
+                .build();
+
+        Game game = FENDecoder.loadGame(FENDecoder.INITIAL_FEN);
+
+        searchResult = moveFinder.search(game, 1);
+
+        int[] visitedNodesCounters = searchResult.getRegularNodeStatistics().visitedNodesCounters();
+        int visitedNodesCountersTotal = Arrays.stream(visitedNodesCounters).sum();
+        int[] visitedQNodesCounters = searchResult.getQuiescenceNodeStatistics().visitedNodesCounters();
+        int visitedQNodesCountersTotal = Arrays.stream(visitedQNodesCounters).sum();
+        int executedMoves = searchResult.getExecutedMoves();
+
+        assertEquals(20, visitedNodesCounters[0]);
+        assertEquals(20, visitedNodesCountersTotal);
+        assertEquals(0, visitedQNodesCounters[0]);
+        assertEquals(0, visitedQNodesCountersTotal);
+        assertEquals(20, executedMoves);
+    }
+
+    @Test
+    public void testSearch_04() {
+        SearchMove moveFinder = new AlphaBetaBuilder()
+                .withGameEvaluator(new EvaluatorByMaterial())
+                .withQuiescence()
+                .withTranspositionTable()
+                .withQTranspositionTable()
+                .withStatistics()
+                .build();
+
+        Game game = FENDecoder.loadGame(FENDecoder.INITIAL_FEN);
+
+        searchResult = moveFinder.search(game, 1);
+
+        int[] visitedNodesCounters = searchResult.getRegularNodeStatistics().visitedNodesCounters();
+        int visitedNodesCountersTotal = Arrays.stream(visitedNodesCounters).sum();
+        int[] visitedQNodesCounters = searchResult.getQuiescenceNodeStatistics().visitedNodesCounters();
+        int visitedQNodesCountersTotal = Arrays.stream(visitedQNodesCounters).sum();
+        int executedMoves = searchResult.getExecutedMoves();
+
+        assertEquals(20, visitedNodesCounters[0]);
+        assertEquals(20, visitedNodesCountersTotal);
+        assertEquals(0, visitedQNodesCounters[0]);
+        assertEquals(0, visitedQNodesCountersTotal);
+        assertEquals(61, executedMoves); // Ver mas arriba explicacion
+
     }
 }
