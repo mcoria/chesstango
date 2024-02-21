d88f60b2a7c753df819bb25c4932f3caeb5e922d
Mauricio Coria <coriamauricio@gmail.com>
2024-02-21 14:57:12 -0300

    CHT-242: Adding module engine-tools

--- board/src/test/java/net/chesstango/PerftTestSuite.java
+++ board/src/test/java/net/chesstango/PerftTestSuite.java
@@ -3,20 +3,17 @@ package net.chesstango;
 import net.chesstango.board.perft.InitialPositionTest;
 import net.chesstango.board.perft.KiwipeteTest;
 import net.chesstango.board.perft.PerftDifferentPositionsTest;
-import net.chesstango.board.perft.PerftMainTestSuiteTest;
 import net.chesstango.board.perft.imp.PerftWithMapIterateDeepingTest;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 /**
  * @author Mauricio Coria
- *
  */
 @Suite
 @SelectClasses({
         InitialPositionTest.class,
         KiwipeteTest.class,
-        PerftMainTestSuiteTest.class,
         PerftDifferentPositionsTest.class,
         PerftWithMapIterateDeepingTest.class
 })
