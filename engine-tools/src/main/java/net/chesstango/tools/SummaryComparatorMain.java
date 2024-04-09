e24e341b72017d236d431412d0631ee8c8c2d8ce
Mauricio Coria <coriamauricio@gmail.com>
2024-04-09 00:09:15 -0300

    CHT-253: Adding wrapper and fixing bug

--- engine-tools/src/main/java/net/chesstango/tools/SummaryComparatorMain.java
+++ engine-tools/src/main/java/net/chesstango/tools/SummaryComparatorMain.java
@@ -20,12 +20,14 @@ import java.util.Objects;
 public class SummaryComparatorMain {
 
     public static void main(String[] args) {
-        SummaryComparatorMain summaryComparatorMain = new SummaryComparatorMain("depth-5-2024-02-27-01-39-v0.0.26");
+        SummaryComparatorMain summaryComparatorMain = new SummaryComparatorMain("depth-6-2024-02-27-00-53-v0.0.26");
         //summaryComparatorMain.addSession("depth-5-2024-03-02-11-23-CHT-254");
-        summaryComparatorMain.addSession("depth-5-2024-03-04-09-30-CHT-156");
-        summaryComparatorMain.addSession("depth-5-2024-04-08-13-53-CHT-253");
-        summaryComparatorMain.addSession("depth-5-2024-04-08-22-51-CHT-253");
-        //depth-5-2024-04-08-22-51-CHT-253
+        summaryComparatorMain.addSession("depth-6-2024-03-02-11-43-CHT-254");
+        summaryComparatorMain.addSession("depth-6-2024-03-03-22-25-CHT-256");
+        summaryComparatorMain.addSession("depth-6-2024-03-04-09-55-CHT-156");
+        summaryComparatorMain.addSession("depth-6-2024-04-08-23-09-CHT-253");
+        //
+        //
 
         summaryComparatorMain.execute("C:\\java\\projects\\chess\\chess-utils\\testing\\positions\\database\\mate-w1.epd");
         summaryComparatorMain.execute("C:\\Java\\projects\\chess\\chess-utils\\testing\\positions\\database\\mate-b1.epd");
