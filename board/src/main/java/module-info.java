77901d6d9b0f1622cdf56d29d85cfad9e05a44b8
Mauricio Coria <coriamauricio@gmail.com>
2024-04-20 17:01:52 -0300

    CHT-262: Refactoring

--- board/src/main/java/module-info.java
+++ board/src/main/java/module-info.java
@@ -9,13 +9,9 @@ module net.chesstango.board {
     exports net.chesstango.board.representations.ascii;
     exports net.chesstango.board.position;
     exports net.chesstango.board.perft;
+    exports net.chesstango.board.perft.imp;
     exports net.chesstango.board.movesgenerators.pseudo;
-    exports net.chesstango.board.movesgenerators.pseudo.strategies;
-    exports net.chesstango.board.movesgenerators.legal;
-    exports net.chesstango.board.movesgenerators.legal.strategies;
-    exports net.chesstango.board.movesgenerators.legal.squarecapturers;
     exports net.chesstango.board.moves;
-    exports net.chesstango.board.moves.factories;
     exports net.chesstango.board.moves.containers;
     exports net.chesstango.board.iterators;
     exports net.chesstango.board.iterators.bysquare;
@@ -23,6 +19,7 @@ module net.chesstango.board {
     exports net.chesstango.board.factory;
     exports net.chesstango.board.builders;
     exports net.chesstango.board.analyzer;
+    exports net.chesstango.board.moves.factories;
 
     requires static lombok;
 }
