fa3f83c68b2fe07e9ed5c2606fceb7cf63650a86
Mauricio Coria <coriamauricio@gmail.com>
2025-04-17 18:34:00 -0300

    Refactor: Replace SyzygyConstants.Table with TableType enum for improved type safety and clarity

--- board/src/main/java/net/chesstango/board/representations/syzygy/TableData.java
+++ board/src/main/java/net/chesstango/board/representations/syzygy/TableData.java
@@ -1,14 +1,5 @@
 package net.chesstango.board.representations.syzygy;
 
-import java.io.IOException;
-import java.nio.ByteOrder;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.EnumSet;
-
 import static net.chesstango.board.representations.syzygy.SyzygyConstants.Encoding.*;
 import static net.chesstango.board.representations.syzygy.TableType.DTM;
 import static net.chesstango.board.representations.syzygy.TableType.DTZ;
@@ -16,20 +7,21 @@ import static net.chesstango.board.representations.syzygy.TableType.DTZ;
 class TableData {
     final BaseEntry baseEntry;
     final TableType table;
+    final TableFile tableFile;
 
-    FileChannel channel;
-    MappedByteBuffer buffer;
     boolean ready;
     boolean error;
 
     public TableData(BaseEntry baseEntry, TableType table) {
         this.baseEntry = baseEntry;
         this.table = table;
+        this.tableFile = new TableFile();
     }
 
     public int probe_table(long key) {
         if (!ready) {
-            if (!init_table()) {
+            if (!error && !init_table()) {
+                error = true;
                 return 0;
             }
             ready = true;
@@ -38,17 +30,16 @@ class TableData {
     }
 
     boolean init_table() {
-
-        if (map_tb(table.getSuffix())) {
+        if (tableFile.map_tb(baseEntry.syzygy.path, baseEntry.tableName, table.getSuffix())) {
 
             /**
              * The main header of the tablebases file:
              * bytes 0-3: magic number
              */
-            int magicNumber = read_le_u32(0);
+            int magicNumber = tableFile.read_le_u32(0);
 
             if (magicNumber != table.getMagicNumber()) {
-                throw new RuntimeException("Corrupted file");
+                return false;
             }
 
             long dataPtr = 0;
@@ -60,7 +51,7 @@ class TableData {
              *      bit 1 is set for a pawnful table.
              *      bits 4-7: number of pieces N (N=5 for KRPvKR)
              */
-            byte byte4 = read_uint8_t(4);
+            byte byte4 = tableFile.read_uint8_t(4);
 
             boolean nonSymmetric = (byte4 & 0b00000001) != 0; //bit 0 is set for a non-symmetric table, i.e. separate wtm and btm.
             boolean pawnfulTable = (byte4 & 0b00000010) != 0; //bit 1 is set for a pawnful table.
@@ -85,37 +76,10 @@ class TableData {
                 //dataPtr += be.num + 1 + (be.hasPawns() && be.pawns[1] != 0);
             }
 
-
             return true;
         }
 
         return false;
-    }
-
-    boolean map_tb(String suffix) {
-        Path pathToRead = Path.of(baseEntry.syzygy.path, String.format("%s%s", baseEntry.tableName, suffix));
-        try {
-
-            channel = (FileChannel) Files.newByteChannel(pathToRead, EnumSet.of(StandardOpenOption.READ));
-
-            buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
-
-            buffer.order(ByteOrder.LITTLE_ENDIAN);
-
-            return true;
-
-        } catch (IOException e) {
-            error = true;
-        }
-        return false;
-    }
-
-    int read_le_u32(int idx) {
-        return buffer.getInt();
-    }
-
-    public byte read_uint8_t(int idx) {
-        return buffer.get(idx);
     }
 
     private int init_enc_info(BaseEntry.EncInfo encInfo, long dataPtr, int i, int t, SyzygyConstants.Encoding enc) {
---
+++ board/src/main/java/net/chesstango/board/representations/syzygy/TableFile.java
@@ -0,0 +1,44 @@
+package net.chesstango.board.representations.syzygy;
+
+import java.io.IOException;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.EnumSet;
+
+/**
+ * @author Mauricio Coria
+ */
+class TableFile {
+    FileChannel channel;
+    MappedByteBuffer buffer;
+
+    boolean map_tb(String basePath, String fileName, String suffix) {
+        Path pathToRead = Path.of(basePath, String.format("%s%s", fileName, suffix));
+        try {
+
+            channel = (FileChannel) Files.newByteChannel(pathToRead, EnumSet.of(StandardOpenOption.READ));
+
+            buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+
+            buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+            return true;
+
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+        return false;
+    }
+
+    int read_le_u32(int idx) {
+        return buffer.getInt();
+    }
+
+    public byte read_uint8_t(int idx) {
+        return buffer.get(idx);
+    }
+}
