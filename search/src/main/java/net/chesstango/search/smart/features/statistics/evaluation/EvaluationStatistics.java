5b8ec08d853874e4eb3584bb224da94d12ee15c7
Mauricio Coria <coriamauricio@gmail.com>
2025-09-18 10:19:48 -0300

    Remove redundant `logback-core` dependency, mark statistics records as `Serializable`, update `gardel.version` to 1.0.12, and enhance Maven Shade plugin configuration.

--- search/src/main/java/net/chesstango/search/smart/features/statistics/evaluation/EvaluationStatistics.java
+++ search/src/main/java/net/chesstango/search/smart/features/statistics/evaluation/EvaluationStatistics.java
@@ -1,9 +1,10 @@
 package net.chesstango.search.smart.features.statistics.evaluation;
 
+import java.io.Serializable;
 import java.util.Set;
 
 /**
  * @author Mauricio Coria
  */
-public record EvaluationStatistics(long evaluationsCounter, long cacheHitsCounter, Set<EvaluationEntry> evaluations) {
+public record EvaluationStatistics(long evaluationsCounter, long cacheHitsCounter, Set<EvaluationEntry> evaluations) implements Serializable {
 }
