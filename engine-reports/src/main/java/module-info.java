8acedd6e665d4b1121af3a99089cad55c80c0f36
Mauricio Coria <coriamauricio@gmail.com>
2025-10-21 23:38:24 -0300

    Add export for `net.chesstango.reports.engine` in module-info for enhanced modularization and access

--- engine-reports/src/main/java/module-info.java
+++ engine-reports/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module net.chesstango.reports {
     exports net.chesstango.reports.tree.nodes;
     exports net.chesstango.reports.tree.pv;
     exports net.chesstango.reports.tree;
+    exports net.chesstango.reports.engine;
 
     requires net.chesstango.search;
     requires net.chesstango.board;
