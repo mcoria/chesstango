f24a5232188f67f3402ba4eb5b634e7e74747c98
Mauricio Coria <coriamauricio@gmail.com>
2026-01-28 19:00:11 -0300

    Improve logging for state changes by handling null states and add `isReady` command support in `WaitCmdGoState`.

---
+++ engine/src/main/java/net/chesstango/engine/SearchInvokerAbstract.java
@@ -0,0 +1,61 @@
+package net.chesstango.engine;
+
+import lombok.extern.slf4j.Slf4j;
+import net.chesstango.board.Game;
+import net.chesstango.board.representations.move.SimpleMoveEncoder;
+import net.chesstango.search.PrincipalVariation;
+import net.chesstango.search.SearchResultByDepth;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * @author Mauricio Coria
+ */
+@Slf4j
+abstract class SearchInvokerAbstract implements SearchInvoker {
+    private final SearchByChain searchByChain;
+    private final SimpleMoveEncoder simpleMoveEncoder = new SimpleMoveEncoder();
+
+    SearchInvokerAbstract(SearchByChain searchByChain) {
+        this.searchByChain = searchByChain;
+    }
+
+    SearchResponse search(Game game, int depth, Predicate<SearchResultByDepth> searchPredicate, SearchListener searchListener) {
+        // Executes and handles exceptions during the search
+        try {
+            searchListener.searchStarted();
+
+            SearchContext context = new SearchContext()
+                    .setGame(game)
+                    .setDepth(depth)
+                    .setSearchResultByDepthPredicate(searchPredicate)
+                    .setSearchResultByDepthConsumer(wrapSearchListener(searchListener));
+
+            SearchResponse searchResult = searchByChain.search(context);
+
+            searchListener.searchFinished(searchResult);
+
+            return searchResult;
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Consumer<SearchResultByDepth> wrapSearchListener(SearchListener searchListener) {
+        return searchResultByDepth -> {
+            String pv = simpleMoveEncoder
+                    .encodeMoves(searchResultByDepth
+                            .getPrincipalVariation()
+                            .stream()
+                            .map(PrincipalVariation::move)
+                            .toList()
+                    );
+            String infoStr = String.format("depth %d seldepth %d pv %s", searchResultByDepth.getDepth(), searchResultByDepth.getDepth(), pv);
+
+            searchListener.searchInfo(infoStr);
+        };
+    }
+}
--- engine/src/main/java/net/chesstango/engine/SearchInvokerSync.java
+++ engine/src/main/java/net/chesstango/engine/SearchInvokerSync.java
@@ -1,60 +1,28 @@
 package net.chesstango.engine;
 
+import lombok.extern.slf4j.Slf4j;
 import net.chesstango.board.Game;
-import net.chesstango.board.representations.move.SimpleMoveEncoder;
-import net.chesstango.search.PrincipalVariation;
 import net.chesstango.search.SearchResultByDepth;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
  * @author Mauricio Coria
  */
-class SearchInvokerSync implements SearchInvoker {
-    private final SearchByChain searchByChain;
-    private final SimpleMoveEncoder simpleMoveEncoder = new SimpleMoveEncoder();
-
+@Slf4j
+class SearchInvokerSync extends SearchInvokerAbstract {
     SearchInvokerSync(SearchByChain searchByChain) {
-        this.searchByChain = searchByChain;
+        super(searchByChain);
     }
 
     @Override
     public Future<SearchResponse> searchImp(Game game, int depth, Predicate<SearchResultByDepth> searchPredicate, SearchListener searchListener) {
-        try {
-            searchListener.searchStarted();
 
-            SearchContext context = new SearchContext()
-                    .setGame(game)
-                    .setDepth(depth)
-                    .setSearchResultByDepthPredicate(searchPredicate)
-                    .setSearchResultByDepthConsumer(wrappSearchListener(searchListener));
+        SearchResponse searchResponse = search(game, depth, searchPredicate, searchListener);
 
-            SearchResponse searchResult = searchByChain.search(context);
-
-            searchListener.searchFinished(searchResult);
-
-            return CompletableFuture.completedFuture(searchResult);
-        } catch (RuntimeException e) {
-            e.printStackTrace(System.err);
-            throw new RuntimeException(e);
-        }
+        return CompletableFuture.completedFuture(searchResponse);
     }
 
-    private Consumer<SearchResultByDepth> wrappSearchListener(SearchListener searchListener) {
-        return searchResultByDepth -> {
-            String pv = simpleMoveEncoder
-                    .encodeMoves(searchResultByDepth
-                            .getPrincipalVariation()
-                            .stream()
-                            .map(PrincipalVariation::move)
-                            .toList()
-                    );
-            String infoStr = String.format("depth %d seldepth %d pv %s", searchResultByDepth.getDepth(), searchResultByDepth.getDepth(), pv);
-
-            searchListener.searchInfo(infoStr);
-        };
-    }
 }
