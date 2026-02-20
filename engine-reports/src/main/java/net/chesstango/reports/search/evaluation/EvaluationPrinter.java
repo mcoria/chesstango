dd625150e8b8890b33743bc61f398b795dba304c
Mauricio Coria <coriamauricio@gmail.com>
2026-02-20 18:42:12 -0300

    Refactor `EvaluationPrinter` to use `PrinterTxtTable` for table generation and improve code readability.

--- engine-reports/src/main/java/net/chesstango/reports/search/evaluation/EvaluationPrinter.java
+++ engine-reports/src/main/java/net/chesstango/reports/search/evaluation/EvaluationPrinter.java
@@ -3,9 +3,11 @@ package net.chesstango.reports.search.evaluation;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import net.chesstango.reports.Printer;
+import net.chesstango.reports.PrinterTxtTable;
 
 import java.io.PrintStream;
-import java.util.Objects;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
 
 /**
  * @author Mauricio Coria
@@ -41,104 +43,29 @@ class EvaluationPrinter implements Printer {
 
 
     EvaluationPrinter printDetails() {
-        out.printf("Evaluation Statistics\n");
+        out.printf("Evaluation Statistics%n");
 
-        int longestId = 0;
-        for (int i = 0; i < reportModel.moveDetails.size(); i++) {
-            String epdId = reportModel.moveDetails.get(i).id;
-            if (Objects.nonNull(epdId) && epdId.length() > longestId) {
-                longestId = epdId.length();
-            }
-        }
+        PrinterTxtTable printerTxtTable = new PrinterTxtTable(6).setOut(out);
 
-        // Marco superior de la tabla
-        out.printf(" ________");
-        //out.printf("____________");
-        out.printf("______________");
-        out.printf("______________");
-        out.printf("_______________");
-        out.printf("_______________");
-        out.printf("____________________");
-        if (longestId > 0) {
-            out.printf(" %s", "_".repeat(longestId + 2));
-        }
-        out.printf("\n");
+        printerTxtTable.setTitles("Move", "Evaluations", "Cache Hits", "Positions", "Values", "Collisions");
+        reportModel.moveDetails.forEach(moveDetail -> {
 
-        // Nombre de las columnas
-        out.printf("| Move   ");
-        //out.printf("| Collisions ");
-        out.printf("| Evaluations ");
-        out.printf("| Cache Hits  ");
-        out.printf("| Positions    ");
-        out.printf("| Values       ");
-        out.printf("| Collisions        ");
-        if (longestId > 0) {
-            out.printf("| ID");
-            out.printf(" ".repeat(longestId - 1));
-        }
-        out.printf("|\n");
+            printerTxtTable.addRow(moveDetail.move,
+                    Long.toString(moveDetail.evaluationCounter),
+                    Long.toString(moveDetail.evaluationsCacheHitCounter),
+                    Long.toString(moveDetail.evaluationPositionCounter),
+                    Long.toString(moveDetail.evaluationValueCounter),
+                    String.format("%d (%2d%%) ", moveDetail.evaluationPositionValueCollisionsCounter, moveDetail.evaluationPositionValueCollisionsPercentage));
+        });
 
-        // Cuerpo
-        for (EvaluationModel.EvaluationReportModelDetail moveDetail : reportModel.moveDetails) {
-            out.printf("| %6s ", moveDetail.move);
-            //out.printf("| %10d ", moveDetail.possibleCollisionsCounter);
-            out.printf("| %11d ", moveDetail.evaluationCounter);
-            out.printf("| %11d ", moveDetail.evaluationsCacheHitCounter);
-            out.printf("| %12d ", moveDetail.evaluationPositionCounter);
-            out.printf("| %12d ", moveDetail.evaluationValueCounter);
-            out.printf("| %11d (%2d%%) ", moveDetail.evaluationPositionValueCollisionsCounter, moveDetail.evaluationPositionValueCollisionsPercentage);
-            if (longestId > 0) {
-                out.printf("| %" + longestId + "s ", moveDetail.id);
-            }
-            out.printf("|\n");
-        }
+        printerTxtTable.setBottomRow("SUM",
+                Long.toString(reportModel.evaluationCounterTotal),
+                Long.toString(reportModel.evaluationsCacheHitCounterTotal),
+                Long.toString(reportModel.evaluationPositionCounterTotal),
+                Long.toString(reportModel.evaluationValueCounterTotal),
+                String.format("%d (%2d%%) ", reportModel.evaluationPositionValueCollisionsCounterTotal, reportModel.evaluationCollisionPercentageTotal));
 
-        // Totales
-        out.printf("|--------");
-        // out.printf("|------------");
-        out.printf("|-------------");
-        out.printf("|-------------");
-        out.printf("|--------------");
-        out.printf("|--------------");
-        out.printf("|-------------------");
-        if (longestId > 0) {
-            out.printf("|");
-            out.printf("-".repeat(longestId + 2));
-        }
-        out.printf("|\n");
-
-
-        out.printf("| SUM    ");
-        //out.printf("|            ");
-        out.printf("| %11d ", reportModel.evaluationCounterTotal);
-        out.printf("| %11d ", reportModel.evaluationsCacheHitCounterTotal);
-        out.printf("| %12d ", reportModel.evaluationPositionCounterTotal);
-        out.printf("| %12d ", reportModel.evaluationValueCounterTotal);
-        out.printf("| %11d (%2d%%) ", reportModel.evaluationPositionValueCollisionsCounterTotal, reportModel.evaluationCollisionPercentageTotal);
-        if (longestId > 0) {
-            out.printf("|");
-            out.printf(" ".repeat(longestId + 2));
-        }
-        out.printf("|\n");
-
-
-        // Marco inferior de la tabla
-        out.printf(" ---------");
-        //out.printf("--------------");
-        out.printf("--------------");
-        out.printf("--------------");
-        out.printf("---------------");
-        out.printf("---------------");
-        ;
-        out.printf("------------------- ");
-        if (longestId > 0) {
-            out.printf("%s", "-".repeat(longestId + 2));
-        }
-        out.printf("\n");
-
-        //out.printf("Collisions = posibles colisiones (puede o no serlo) \n");
-
-        out.printf("\n");
+        printerTxtTable.print();
 
         return this;
     }
