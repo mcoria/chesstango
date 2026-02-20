cbf9445706eae2932b47fa85603c1b15037417f9
Mauricio Coria <coriamauricio@gmail.com>
2026-02-20 18:18:53 -0300

    Refactor `CutoffPrinter` and `HeaderPrinter` to use `PrinterTxtTable` for table generation and enhance code readability.

--- engine-reports/src/main/java/net/chesstango/reports/search/nodes/CutoffPrinter.java
+++ engine-reports/src/main/java/net/chesstango/reports/search/nodes/CutoffPrinter.java
@@ -3,14 +3,17 @@ package net.chesstango.reports.search.nodes;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import net.chesstango.reports.Printer;
+import net.chesstango.reports.PrinterTxtTable;
 
 import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 /**
  * @author Mauricio Coria
  */
-class CutoffPrinter  implements Printer {
+class CutoffPrinter implements Printer {
     @Setter
     @Accessors(chain = true)
     private NodesModel reportModel;
@@ -23,49 +26,38 @@ class CutoffPrinter  implements Printer {
     public CutoffPrinter print() {
         out.printf("Cutoff per search level (higher is better)\n");
 
-        // Marco superior de la tabla
-        out.printf(" ________");
-        IntStream.range(0, reportModel.maxSearchRLevel).forEach(depth -> out.printf("____________"));
-        IntStream.range(0, reportModel.maxSearchQLevel).forEach(depth -> out.printf("____________"));
-        out.printf("____________");
-        out.printf("\n");
+        PrinterTxtTable printerTxtTable = new PrinterTxtTable(2 + reportModel.maxSearchRLevel + reportModel.maxSearchQLevel).setOut(out);
 
-        // Nombre de las columnas
-        out.printf("| Move   ");
-        IntStream.range(0, reportModel.maxSearchRLevel).forEach(depth -> out.printf("| RLevel %2d ", depth + 1));
-        IntStream.range(0, reportModel.maxSearchQLevel).forEach(depth -> out.printf("| QLevel %2d ", depth + 1));
-        out.printf("| Cutoff    ");
-        out.printf("|\n");
+        List<String> tmp = new LinkedList<>();
+        tmp.add("Move");
+        IntStream.range(0, reportModel.maxSearchRLevel).mapToObj(depth -> String.format("RLevel %2d", depth + 1)).forEach(tmp::add);
+        IntStream.range(0, reportModel.maxSearchQLevel).mapToObj(depth -> String.format("QLevel %2d", depth + 1)).forEach(tmp::add);
+        tmp.add("Cutoff");
 
-        // Cuerpo
-        for (NodesModel.NodesModelDetail moveDetail : reportModel.nodesModelDetails) {
-            out.printf("| %6s ", moveDetail.move);
-            IntStream.range(0, reportModel.maxSearchRLevel).forEach(depth -> out.printf("|  %6d %% ", moveDetail.cutoffRPercentages[depth]));
-            IntStream.range(0, reportModel.maxSearchQLevel).forEach(depth -> out.printf("|  %6d %% ", moveDetail.cutoffQPercentages[depth]));
-            out.printf("|  %6d %% ", moveDetail.cutoffPercentageTotal);
-            out.printf("|\n");
-        }
+        printerTxtTable.setTitles(tmp.toArray(new String[0]));
 
-        // Totales
-        out.printf("|--------");
-        IntStream.range(0, reportModel.maxSearchRLevel).forEach(depth -> out.printf("|-----------"));
-        IntStream.range(0, reportModel.maxSearchQLevel).forEach(depth -> out.printf("|-----------"));
-        out.printf("|-----------");
-        out.printf("|\n");
+        reportModel.nodesModelDetails.forEach(moveDetail -> {
+            List<String> tmpRow = new LinkedList<>();
 
-        out.printf("| SUM    ");
-        IntStream.range(0, reportModel.maxSearchRLevel).forEach(depth -> out.printf("|  %6d %% ", reportModel.cutoffRPercentages[depth]));
-        IntStream.range(0, reportModel.maxSearchQLevel).forEach(depth -> out.printf("|  %6d %% ", reportModel.cutoffQPercentages[depth]));
-        out.printf("|  %6d %% ", reportModel.cutoffPercentageTotal);
-        out.printf("|\n");
+            tmpRow.add(String.format("%s", moveDetail.move));
 
+            IntStream.range(0, reportModel.maxSearchRLevel).mapToObj(depth -> String.format("%d %%", moveDetail.cutoffRPercentages[depth])).forEach(tmpRow::add);
+            IntStream.range(0, reportModel.maxSearchQLevel).mapToObj(depth -> String.format("%d %%", moveDetail.cutoffQPercentages[depth])).forEach(tmpRow::add);
 
-        // Marco inferior de la tabla
-        out.printf(" --------");
-        IntStream.range(0, reportModel.maxSearchRLevel).forEach(depth -> out.printf("------------"));
-        IntStream.range(0, reportModel.maxSearchQLevel).forEach(depth -> out.printf("------------"));
-        out.printf("------------");
-        out.printf("\n\n");
+            tmpRow.add(String.format("%d %%", moveDetail.cutoffPercentageTotal));
+
+            printerTxtTable.addRow(tmpRow.toArray(new String[0]));
+        });
+
+        tmp = new LinkedList<>();
+        tmp.add("SUM");
+        IntStream.range(0, reportModel.maxSearchRLevel).mapToObj(depth -> String.format("%d %%", reportModel.cutoffRPercentages[depth])).forEach(tmp::add);
+        IntStream.range(0, reportModel.maxSearchQLevel).mapToObj(depth -> String.format("%d %%", reportModel.cutoffQPercentages[depth])).forEach(tmp::add);
+        tmp.add(String.format("%d %%", reportModel.cutoffPercentageTotal));
+
+        printerTxtTable.setBottomRow(tmp.toArray(new String[0]));
+
+        printerTxtTable.print();
 
         return this;
     }
