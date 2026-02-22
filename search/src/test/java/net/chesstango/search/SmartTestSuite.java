c0d306785f78cee4561185354273b78a304eca65
Mauricio Coria <coriamauricio@gmail.com>
2026-02-22 10:04:43 -0300

    Remove `MoveSelector` class and associated tests.

--- search/src/test/java/net/chesstango/search/SmartTestSuite.java
+++ search/src/test/java/net/chesstango/search/SmartTestSuite.java
@@ -1,7 +1,6 @@
 package net.chesstango.search;
 
 import net.chesstango.search.smart.IterativeDeepeningTest;
-import net.chesstango.search.smart.MoveSelectorTest;
 import net.chesstango.search.smart.alphabeta.transposition.TranspositionEntryTest;
 import net.chesstango.search.visitors.ChainPrinterVisitorTest;
 import org.junit.platform.suite.api.SelectClasses;
@@ -13,7 +12,6 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @SelectClasses({
         IterativeDeepeningTest.class,
-        MoveSelectorTest.class,
         TranspositionEntryTest.class,
         RepetitionTest.class,
         ChainPrinterVisitorTest.class
