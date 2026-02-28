35a84fed256cad422cd543098bd3788d30148146
Mauricio Coria <coriamauricio@gmail.com>
2026-02-28 06:36:41 -0300

    Refactor `TTableArrayLoad` and `TTableArraySave` benchmarks: add detailed teardown and iteration statistics, adjust table size, and introduce additional load/save scenarios.

--- engine-reports/src/main/java/net/chesstango/reports/jmh/TTableArrayLoad.java
+++ engine-reports/src/main/java/net/chesstango/reports/jmh/TTableArrayLoad.java
@@ -15,27 +15,36 @@ import java.util.concurrent.TimeUnit;
 @Measurement(iterations = 20, time = 5000, timeUnit = TimeUnit.MILLISECONDS)
 public class TTableArrayLoad {
 
+    private static final int ARRAY_SIZE = 1024 * 512;
+
     private TTable tTable;
     private TranspositionEntry transposition;
     private Random random;
 
     private static final TranspositionBound[] BOUNDS = TranspositionBound.values();
     private static final int BOUNDS_SIZE = BOUNDS.length;
+    private long loadCalls;
+    private long founds;
 
     @Setup(Level.Trial)
     public void setUp() {
         random = new Random();
-        //tTable = new TTableArrayPrimitives();
-        tTable = new TTableArrayObj();
+        tTable = new TTableArrayPrimitives();
+        //tTable = new TTableArrayObj();
         //tTable = new TTableMap();
         transposition = new TranspositionEntry();
     }
 
+    /**
+     * La tabla se encuentra con datos al 50%
+     */
     @Setup(Level.Iteration)
     public void fillTranspositionTable() {
+        loadCalls = 0;
+        founds = 0;
         tTable.clear();
-        for (int i = 0; i < 1024 * 10; i++) {
-            transposition.setHash(random.nextLong());
+        for (int i = 0; i < ARRAY_SIZE; i += 2) {
+            transposition.setHash(i);
             transposition.setDraft(random.nextInt());
             transposition.setMove((short) random.nextInt());
             transposition.setValue(random.nextInt());
@@ -45,10 +54,33 @@ public class TTableArrayLoad {
     }
 
     @Benchmark
-    public void benchmarkLoad(Blackhole blackhole) {
-        boolean found = tTable.load(random.nextLong(), transposition);
+    public void benchmarkLoad_6pct(Blackhole blackhole) {
+        boolean found = tTable.load(random.nextLong((ARRAY_SIZE * 100) / (6 * 2)), transposition);
+
+        loadCalls++;
+        if (found) {
+            founds++;
+        }
 
         // Pass the result to the blackhole to prevent dead code elimination
         blackhole.consume(found);
+    }
+
+    @Benchmark
+    public void benchmarkLoad_9pct(Blackhole blackhole) {
+        boolean found = tTable.load(random.nextLong((ARRAY_SIZE * 100) / (9 * 2)), transposition);
+
+        loadCalls++;
+        if (found) {
+            founds++;
+        }
+
+        // Pass the result to the blackhole to prevent dead code elimination
+        blackhole.consume(found);
+    }
+
+    @TearDown(Level.Iteration)
+    public void teardown() {
+        System.out.printf("Load calls: %d, founds: %d (%d%%). ", loadCalls, founds, founds * 100 / loadCalls);
     }
 }
--- engine-reports/src/main/java/net/chesstango/reports/jmh/TTableArraySave.java
+++ engine-reports/src/main/java/net/chesstango/reports/jmh/TTableArraySave.java
@@ -14,13 +14,21 @@ import java.util.concurrent.TimeUnit;
 @Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 20, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 public class TTableArraySave {
+    private static final int ARRAY_SIZE = 1024 * 512;
 
     private TTable tTable;
-    private TranspositionEntry transposition;
+    private TranspositionEntry transpositionRead;
+    private TranspositionEntry transpositionWrite;
     private Random random;
 
     private static final TranspositionBound[] BOUNDS = TranspositionBound.values();
     private static final int BOUNDS_SIZE = BOUNDS.length;
+    private long hashToWrite;
+    private int updatedCounter;
+    private int insertedCounter;
+    private int overWrittenCounter;
+    private int saveCounter;
+
 
     @Setup(Level.Trial)
     public void setUp() {
@@ -28,28 +36,70 @@ public class TTableArraySave {
         tTable = new TTableArrayPrimitives();
         //tTable = new TTableArrayObj();
         //tTable = new TTableMap();
-        transposition = new TranspositionEntry();
+        transpositionRead = new TranspositionEntry();
+        transpositionWrite = new TranspositionEntry();
     }
 
     @Setup(Level.Iteration)
     public void clearTranspositionTable() {
         tTable.clear();
+        updatedCounter = 0;
+        insertedCounter = 0;
+        overWrittenCounter = 0;
+        saveCounter = 0;
+        for (int i = 0; i < ARRAY_SIZE; i += 2) {
+            transpositionWrite.setHash(i);
+            transpositionWrite.setDraft(random.nextInt());
+            transpositionWrite.setMove((short) random.nextInt());
+            transpositionWrite.setValue(random.nextInt());
+            transpositionWrite.setBound(BOUNDS[random.nextInt(BOUNDS_SIZE)]);
+            tTable.save(transpositionWrite);
+        }
     }
 
     @Setup(Level.Invocation)
-    public void setUpTransposition() {
-        transposition.setHash(random.nextLong());
-        transposition.setDraft(random.nextInt());
-        transposition.setMove((short) random.nextInt());
-        transposition.setValue(random.nextInt());
-        transposition.setBound(BOUNDS[random.nextInt(BOUNDS_SIZE)]);
+    public void takeBackup() {
+        hashToWrite = random.nextLong((ARRAY_SIZE * 100) / (6 * 2));
+
+        tTable.load(hashToWrite % ARRAY_SIZE, transpositionRead);
+
+        transpositionWrite.setHash(hashToWrite);
+        transpositionWrite.setDraft(random.nextInt());
+        transpositionWrite.setMove((short) random.nextInt());
+        transpositionWrite.setValue(random.nextInt());
+        transpositionWrite.setBound(BOUNDS[random.nextInt(BOUNDS_SIZE)]);
     }
 
     @Benchmark
     public void benchmarkSaves(Blackhole blackhole) {
-        TTable.SaveResult result = tTable.save(transposition);
+        TTable.SaveResult result = tTable.save(transpositionWrite);
+
+        switch (result) {
+            case INSERTED -> insertedCounter++;
+            case UPDATED -> updatedCounter++;
+            case OVER_WRITTEN -> overWrittenCounter++;
+        }
+        saveCounter++;
 
         // Pass the result to the blackhole to prevent dead code elimination
         blackhole.consume(result);
+    }
+
+    @TearDown(Level.Invocation)
+    public void restoreBackup() {
+        tTable.save(transpositionRead);
+    }
+
+    @TearDown(Level.Iteration)
+    public void teardown() {
+        System.out.printf("SAVED: %d, INSERTED: %d (%d%%), UPDATED: %d (%d%%), OVER_WRITTEN: %d (%d%%) ",
+                saveCounter,
+                insertedCounter,
+                insertedCounter * 100 / saveCounter,
+                updatedCounter,
+                updatedCounter * 100 / saveCounter,
+                overWrittenCounter,
+                overWrittenCounter * 100 / saveCounter
+        );
     }
 }
