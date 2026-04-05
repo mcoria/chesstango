02b11d1ebc4ec1ba3d19fe6dbb416694fd181e51
Mauricio Coria <coriamauricio@gmail.com>
2026-04-05 09:30:44 -0300

    Added `NodeGroupSorter` and `GroupSorter` to enable group-based move sorting; implemented `NoQuietGroup` as a non-filtering group sorter.

---
+++ search/src/main/java/net/chesstango/search/smart/sorters/NodeGroupSorter.java
@@ -0,0 +1,46 @@
+package net.chesstango.search.smart.sorters;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.chesstango.board.Game;
+import net.chesstango.board.moves.Move;
+import net.chesstango.board.moves.containers.MoveContainerReader;
+import net.chesstango.search.Visitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Mauricio Coria
+ */
+public class NodeGroupSorter implements MoveSorter {
+
+
+    @Setter
+    private Game game;
+
+    @Getter
+    @Setter
+    private GroupSorter groupSorter;
+
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    @Override
+    public Iterable<Move> getOrderedMoves(final int currentPly) {
+        MoveContainerReader<? extends Move> moves = game.getPossibleMoves();
+
+        List<Move> moveList = new ArrayList<>(moves.size());
+
+        for (Move move : moves) {
+            groupSorter.offer(move);
+        }
+
+        groupSorter.collect(moveList);
+
+        return moveList;
+    }
+}
