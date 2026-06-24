2eeac0f383bf9ca6a6070594b90d89644eb71568
Mauricio Coria <coriamauricio@gmail.com>
2026-06-24 09:09:58 -0300

    Update `GameToPGNTest` to modify expected EPD string assertions.

--- board/src/test/java/net/chesstango/board/representations/pgn/GameToPGNTest.java
+++ board/src/test/java/net/chesstango/board/representations/pgn/GameToPGNTest.java
@@ -78,7 +78,7 @@ public class GameToPGNTest {
         List<EPD> pgnToEpd = pgn.toEPD().toList();
 
         assertEquals(1, pgnToEpd.size());
-        assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - hmvc 0; fmvn 1; sm a4; c5 \"result='*'\"; c6 \"clock=1\"; c7 \"totalClock=1\"; id \"1 w\";", pgnToEpd.getFirst().toString());
+        assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - hmvc 0; fmvn 1; sm a4; c5 \"result='*'\"; id \"1 w\";", pgnToEpd.getFirst().toString());
     }
 
 
