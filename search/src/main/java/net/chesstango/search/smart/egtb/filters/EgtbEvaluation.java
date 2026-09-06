81d01a2613a46484617a250dcf019b879bd6e3c2
Mauricio Coria <coriamauricio@gmail.com>
2026-09-06 08:53:18 -0300

    Add game management to `EgtbEvaluation` and update `SetGameVisitor` to support `EgtbEvaluation` handling.

--- search/src/main/java/net/chesstango/search/smart/egtb/filters/EgtbEvaluation.java
+++ search/src/main/java/net/chesstango/search/smart/egtb/filters/EgtbEvaluation.java
@@ -2,6 +2,8 @@ package net.chesstango.search.smart.egtb.filters;
 
 import lombok.Getter;
 import lombok.Setter;
+import net.chesstango.board.Color;
+import net.chesstango.board.Game;
 import net.chesstango.search.Acceptor;
 import net.chesstango.search.Visitor;
 import net.chesstango.search.smart.AlphaBetaFilter;
@@ -16,6 +18,8 @@ public class EgtbEvaluation implements AlphaBetaFilter, Acceptor {
 
     private EndGameTableBase endGameTableBase;
 
+    private Game game;
+
     @Override
     public void accept(Visitor visitor) {
         visitor.visit(this);
@@ -23,6 +27,6 @@ public class EgtbEvaluation implements AlphaBetaFilter, Acceptor {
 
     @Override
     public int alphaBeta(int currentPly, int alpha, int beta) {
-        return endGameTableBase.evaluate();
+        return Color.WHITE.equals(game.getPosition().getCurrentTurn()) ? endGameTableBase.evaluate() : -endGameTableBase.evaluate();
     }
 }
