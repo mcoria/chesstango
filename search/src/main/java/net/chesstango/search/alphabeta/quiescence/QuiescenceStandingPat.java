7c771d182e3e24c57e207731e46417786417d7a2
Mauricio Coria <coriamauricio@gmail.com>
2026-09-16 08:44:39 -0300

    Mark `testEvaluateByMaterialQueen` as disabled in `EvaluatorImp05Test`.

--- search/src/main/java/net/chesstango/search/alphabeta/quiescence/QuiescenceStandingPat.java
+++ search/src/main/java/net/chesstango/search/alphabeta/quiescence/QuiescenceStandingPat.java
@@ -17,8 +17,9 @@ import net.chesstango.search.alphabeta.AlphaBetaFilter;
 public class QuiescenceStandingPat implements AlphaBetaFilter, Acceptor {
 
     //private final int DELTA_MARGIN = 823000;
-    //private final int DELTA_MARGIN = 1645369;
-    private final int DELTA_MARGIN = 440000;
+    //private final int DELTA_MARGIN = 1645369;                           1 pieza diferencia
+    //private final int DELTA_MARGIN = 440000;                      90% - 1 pieza diferencia / mismas piezas
+    private final int DELTA_MARGIN = 540000;                      //95% - 1 pieza diferencia / mismas piezas
 
 
     @Getter
